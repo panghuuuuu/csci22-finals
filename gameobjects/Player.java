84e41c7a9590ec790fff0e7f69280bd6e52adecd
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2022-05-18 02:07:32 +0800

    implemented active-push

--- gameobjects/Player.java
+++ gameobjects/Player.java
@@ -7,9 +7,10 @@ import framework.*;
 
 public class Player extends GameObject {
     private double movementSpeed;
-    private boolean activePush = false, slideX = false, slideY = false;
+    private boolean activePush = false, slideX = false, slideY = false, coolDown = false;
     private String direction = "Right";
     private int pushSpeed;
+    private int coolDownCounter = 0;
 
     public Player(double xPos, double yPos, double w, double h, GameObjectID objectID) {
         super(xPos, yPos, w, h, objectID);
@@ -43,11 +44,13 @@ public class Player extends GameObject {
                 moveY(ySpeed);
                 direction = "Down";
             }
-            if (KeyListener.push) {
+            if (KeyListener.push && !pushCoolDown()) {
                 activePush = true;
             } else {
                 activePush = false;
+                pushCoolDown();
             }
+
 
             if (slideX) {
                if(pushSpeed > 0) {
@@ -81,67 +84,12 @@ public class Player extends GameObject {
             switch(tempObject.getID()) {
                 case PlayerTwo:
                     //Collision
-                    if(getHBounds().intersects(tempObject.getHBounds())) { 
-                        if(tempObject.getXSpeed() < 0) {
-                            x = tempObject.getX() - tempObject.getWidth();
-                        } else if (tempObject.getXSpeed() > 0) {
-                            x = tempObject.getX() + tempObject.getWidth();
-                        }
-                        if(xSpeed > 0) {
-                            x = tempObject.getX() - tempObject.getWidth() + xSpeed;
-                        } else if (xSpeed < 0) {
-                            x = tempObject.getX() + tempObject.getWidth() + xSpeed;
-                        }
-                    }
+                    HorizontalCollision(tempObject);
+                    VerticalCollision(tempObject);
 
-                    if(getVBounds().intersects(tempObject.getVBounds())) {
-                        if(tempObject.getYSpeed() < 0) {
-                            y = tempObject.getY() - tempObject.getHeight();
-                        } else if (tempObject.getYSpeed() > 0) {
-                            y = tempObject.getY() + tempObject.getHeight();
-                        }                    
-                        if(ySpeed > 0) {
-                           y = tempObject.getY() - tempObject.getHeight() + ySpeed;
-                        } else if (ySpeed < 0) {
-                           y = tempObject.getY() + tempObject.getHeight() + ySpeed;
-                        }
-                    }
+                    //Push Mechanic
+                    Pushable(tempObject);
 
-                    if(((Player) tempObject).getHRange().intersects(getHBounds())) {
-                        if(((Player) tempObject).getPush()) {
-                            System.out.println(((Player) tempObject).getDir());
-                            switch(((Player) tempObject).getDir()) {
-                                case "Right":
-                                    pushSpeed = 10;
-                                    slideX = true;
-                                    break;
-                                case "Left":
-                                    pushSpeed = -10;
-                                    slideX = true;
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    }
-
-                    if(((Player) tempObject).getVRange().intersects(getVBounds())) {
-                        if(((Player) tempObject).getPush()) {
-                            System.out.println(((Player) tempObject).getDir());
-                            switch(((Player) tempObject).getDir()) {
-                                case "Down":
-                                    pushSpeed = 10;
-                                    slideY = true;
-                                    break;
-                                case "Up":
-                                    pushSpeed = -10;
-                                    slideY = true;
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    }
                     break;
                 default:
                     break;
@@ -171,6 +119,7 @@ public class Player extends GameObject {
         this.direction = dir;
     }
 
+    //Pushing Range
     public Rectangle getHRange() {
         double vx = this.x - this.width;
         double vy = this.y + 4;
@@ -187,5 +136,88 @@ public class Player extends GameObject {
         double vh = this.height*3;
 
         return new Rectangle((int) vx, (int) vy, (int) vw, (int) vh);
+    }
+
+    //Collision
+    public void HorizontalCollision(GameObject p2) {
+        if(getHBounds().intersects(p2.getHBounds())) { 
+            if(p2.getXSpeed() < 0) {
+                x = p2.getX() - p2.getWidth();
+            } else if (p2.getXSpeed() > 0) {
+                x = p2.getX() + p2.getWidth();
+            }
+            if(xSpeed > 0) {
+                x = p2.getX() - p2.getWidth() + xSpeed;
+            } else if (xSpeed < 0) {
+                x = p2.getX() + p2.getWidth() + xSpeed;
+            }
+        }
+    }
+    public void VerticalCollision(GameObject p2) {
+        if(getVBounds().intersects(p2.getVBounds())) {
+            if(p2.getYSpeed() < 0) {
+                y = p2.getY() - p2.getHeight();
+            } else if (p2.getYSpeed() > 0) {
+                y = p2.getY() + p2.getHeight();
+            }                    
+            if(ySpeed > 0) {
+               y = p2.getY() - p2.getHeight() + ySpeed;
+            } else if (ySpeed < 0) {
+               y = p2.getY() + p2.getHeight() + ySpeed;
+            }
+        }
+    }
+
+    //Push Mechanic
+    public void Pushable(GameObject p2) {
+        if(((Player) p2).getHRange().intersects(getHBounds())) {
+            if(((Player) p2).getPush()) {
+                System.out.println(((Player) p2).getDir());
+                switch(((Player) p2).getDir()) {
+                    case "Right":
+                        pushSpeed = 20;
+                        slideX = true;
+                        break;
+                    case "Left":
+                        pushSpeed = -20;
+                        slideX = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        if(((Player) p2).getVRange().intersects(getVBounds())) {
+            if(((Player) p2).getPush()) {
+                System.out.println(((Player) p2).getDir());
+                switch(((Player) p2).getDir()) {
+                    case "Down":
+                        pushSpeed = 20;
+                        slideY = true;
+                        break;
+                    case "Up":
+                        pushSpeed = -20;
+                        slideY = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private boolean pushCoolDown() {
+        if (activePush) {
+            coolDownCounter = 120; //3 seconds for 60 UPS
+            coolDown = true;
+        } 
+        if (coolDown) {
+            coolDownCounter--;
+        }
+        if (coolDownCounter <= 0) {
+            coolDown = false;
+        }
+        return coolDown;
     }
 }
