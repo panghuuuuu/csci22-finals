3b847d457af15e9674af32bea275b1ca52fc04f3
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2022-05-23 22:14:20 +0800

    added GameObject Comments

--- framework/GameObject.java
+++ framework/GameObject.java
@@ -54,60 +54,69 @@ public abstract class GameObject {
     public abstract void draw(Graphics2D g2d);
 
 
+    /** Moves the GameObject instance's X position by its Speed
+     * @param speed Speed of the GameObject instance**/
     public void moveX(double speed) {
         this.x += speed;
     }
+    /** Moves the GameObject instance's Y position by its Speed
+     * @param speed Speed of the GameObject instance**/
     public void moveY(double speed) {
         this.y += speed;
     }
 
-    
-    //Accessor Methods
+    ////////////////////
+    //ACCESSOR METHODS//
+    ////////////////////
+
+    /** Returns the current X Position of the GameObject instance
+     * @return Current x Value
+     */
     public double getX() {
         return this.x;
     }
+    /** Returns the current Y Position of the GameObject instance
+     * @return Current y value
+     */
     public double getY() {
         return this.y;
     }
+    /** Returns the current width of the GameObject instance
+     * @return current width value
+     */
     public double getWidth() {
         return this.width;
     }
+    /** Returns the current height of the GameObject instance
+     * @return current height value
+     */
     public double getHeight() {
         return this.height;
     }
 
+    /** Returns the GameObject instance ID of the GameObject instance
+     * @return GameObject instance ID
+     */
     public GameObjectID getID() {
         return this.id;
     }
 
+    /** Returns the GameObject instance's Horizontal Speed
+     * @return Current xSpeed value
+     */
     public double getXSpeed() {
         return this.xSpeed;
     }
+    /** Returns the Game Object instance's Vertical Speed
+     * @return Current ySpeed value
+     */
     public double getYSpeed() {
         return this.ySpeed;
     }
 
-    //Mutator Methods
-    public void setX(double xPos) {
-        this.x = xPos;
-    }
-    public void setY(double yPos) {
-        this.y = yPos;
-    }
-
-    public void setXSpeed(double xSpd) {
-        this.xSpeed = xSpd;
-    }
-    public void setYSpeed(double ySpd) {
-        this.ySpeed = ySpd;
-    }
-    public void setWidth(double w) {
-        this.width = w;
-    }
-    public void setHeight(double h) {
-        this.height = h;
-    }
-
+    /** Returns the Game Object instance's Horizontal Bounds
+     * @return {@code Rectangle} of Horizontal Bounds
+     */
     public Rectangle getHBounds() {
         double hx = this.x + this.xSpeed;
         double hy = this.y + 2;
@@ -116,7 +125,9 @@ public abstract class GameObject {
 
         return new Rectangle((int) hx, (int) hy, (int) hw, (int) hh);
     }
-
+    /** Returns the Game Object instance's Vertical Bounds
+     * @return {@code Rectangle} of Vertical Bounds
+     */
     public Rectangle getVBounds() {
         double vx = this.x + 2;
         double vy = this.y + this.ySpeed;
@@ -124,5 +135,48 @@ public abstract class GameObject {
         double vh = this.height + this.ySpeed/2;
 
         return new Rectangle((int) vx, (int) vy, (int) vw, (int) vh);
+    }
+
+    ///////////////////
+    //MUTATOR METHODS//
+    ///////////////////
+
+    /** Sets the X value of GameObject Instance
+     * @param xPos New X Position
+     */
+    public void setX(double xPos) {
+        this.x = xPos;
+    }
+    /** Sets the Y Value of the GameObject Instance
+     * @param yPos New Y Position
+     */
+    public void setY(double yPos) {
+        this.y = yPos;
+    }
+
+    /** Sets the Horizontal Speed of the GameObject Instance
+     * @param xSpd New X Speed value
+     */
+    public void setXSpeed(double xSpd) {
+        this.xSpeed = xSpd;
+    }
+    /** Sets the Vertical Speed of the GameObject Instance
+     * @param ySpd New Y Speed Value
+     */
+    public void setYSpeed(double ySpd) {
+        this.ySpeed = ySpd;
+    }
+
+    /** Sets the Width of the GameObject Instance
+     * @param w New Width Value
+     */
+    public void setWidth(double w) {
+        this.width = w;
+    }
+    /** Sets the Height of the GameObject Instance
+     * @param h New Height Value
+     */
+    public void setHeight(double h) {
+        this.height = h;
     }
 }
